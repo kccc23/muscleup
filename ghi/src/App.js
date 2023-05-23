8e97ba30025b9171c2a2b53ccdef949f45cfd6a4
Sonny <sonnyn.ht@gmail.com>
2023-05-23 17:21:45 -0400

    start navbar component

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,36 +1,18 @@
-import { useEffect, useState } from "react";
-import Construct from "./Construct.js";
-import ErrorNotification from "./ErrorNotification";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Main from "./components/mainpage/Main";
+import Navbar from "./components/navbar/Navbar";
+
 
 function App() {
-  const [launchInfo, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_SAMPLE_SERVICE_API_HOST}/api/launch-details`;
-      console.log("fastapi url: ", url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
-
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, []);
-
   return (
-    <div>
-      <ErrorNotification error={error} />
-      <Construct info={launchInfo} />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <div className="container">
+        <Routes>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ ghi/src/components/navbar/Navbar.js
@@ -0,0 +1,45 @@
+import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+import { ImHome, ImMenu } from "react-icons/im";
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      
+      <div className="navbar-container-image">
+        <ImHome className="navbar-logo"></ImHome>
+        <NavLink className="navbar-title" to="/">
+          MuscleUp
+        </NavLink>
+      </div>
+
+      <div className="right-bar">
+        <div className="navbar-container-links">
+          <ul className="navbar-links">
+            <li className="navbar-item">
+              <NavLink className="navbar-link" to="">
+                Trainers
+              </NavLink>
+            </li>
+            <li className="navbar-item">
+              <NavLink className="navbar-link" to="">
+                Login
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-button">
+          <NavLink className="navbar-link" to="">
+            Muscle UP!
+          </NavLink>
+        </div>
+        <div className="navbar-hamburger-menu">
+          <ImMenu className="navbar-hamburger-menu-icon"></ImMenu>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
