1f91fc2d513008fca9d0349daf81e79f9e6857bd
Sonny <sonnyn.ht@gmail.com>
2023-06-05 13:56:28 -0400

    continued stylization on navbar

--- ghi/src/components/navbar/Navbar.js
+++ ghi/src/components/navbar/Navbar.js
@@ -1,45 +1,106 @@
 import "./Navbar.css";
 import { NavLink } from "react-router-dom";
 import { ImHome, ImMenu } from "react-icons/im";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useGetTokenQuery } from "../../redux-elements/authApi";
 
 function Navbar() {
-  return (
-    <nav className="navbar">
+	const [sideBar, setSideBar] = useState(false);
+	const toggleSideBar = () => setSideBar(!sideBar);
+	const { data } = useGetTokenQuery();
 
-      <div className="navbar-container-image">
-        <ImHome className="navbar-logo"></ImHome>
-        <NavLink className="navbar-title" to="/">
-          MuscleUp
-        </NavLink>
-      </div>
+	useEffect(() => {
+		// Hide Sidebar on Resize
+		const hideMenu = () => {
+			if (window.innerWidth <= 768) {
+				setSideBar(false);
+			}
+		};
 
-      <div className="right-bar">
-        <div className="navbar-container-links">
-          <ul className="navbar-links">
-            <li className="navbar-item">
-              <NavLink className="navbar-link" to="">
-                Trainers
-              </NavLink>
-            </li>
-            <li className="navbar-item">
-              <NavLink className="navbar-link" to="/login">
-                Login
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-button">
-          <NavLink className="navbar-link" to="/signup">
-            Muscle UP!
-          </NavLink>
-        </div>
-        <div className="navbar-hamburger-menu">
-          <ImMenu className="navbar-hamburger-menu-icon"></ImMenu>
-        </div>
-      </div>
-    </nav>
-  );
+		window.addEventListener("resize", hideMenu);
+
+		return () => {
+			window.removeEventListener("resize", hideMenu);
+		};
+	}, []);
+
+	return (
+		<nav className="navbar">
+			<div className="navbar-container-image">
+				<ImHome className="navbar-logo"></ImHome>
+				<NavLink className="navbar-title" to="/">
+					MuscleUp
+				</NavLink>
+			</div>
+
+			<div className="right-bar">
+				<div className="navbar-container-links">
+					<ul className="navbar-links">
+						{data ? (
+							<>
+								<li className="navbar-item">
+									<NavLink className="navbar-link" to="">
+										CSS
+									</NavLink>
+								</li>
+								<li className="navbar-item">
+									<NavLink className="navbar-link" to="/login">
+										SUCKS!!!!
+									</NavLink>
+								</li>
+							</>
+						) : (
+							<>
+								<li className="navbar-item">
+									<NavLink className="navbar-link" to="">
+										Trainers
+									</NavLink>
+								</li>
+								<li className="navbar-item">
+									<NavLink className="navbar-link" to="/login">
+										Login
+									</NavLink>
+								</li>
+							</>
+						)}
+					</ul>
+				</div>
+				{data ? (
+					<div className="navbar-button">
+						<NavLink className="navbar-link" to="/signup">
+							I'M LOGGED IN!!!!!!!!!!!!
+						</NavLink>
+					</div>
+				) : (
+					<div className="navbar-button">
+						<NavLink className="navbar-link" to="/signup">
+							Muscle UP!
+						</NavLink>
+					</div>
+				)}
+				<div className="navbar-hamburger-button">
+					<ImMenu className="navbar-hamburger-menu-icon" onClick={toggleSideBar}></ImMenu>
+				</div>
+
+				{/* Hamburger Menu */}
+				<div className="navSideMenu" style={sideBar ? { right: "0" } : { right: "-100%" }}>
+					<ul className="navSideMenuItems">
+						<NavLink to="/" onClick={toggleSideBar} style={{ textDecoration: "None" }}>
+							<li className="navItemSide">Home</li>
+						</NavLink>
+						<NavLink to="/login" onClick={toggleSideBar} style={{ textDecoration: "None" }}>
+							<li className="navItemSide">Login</li>
+						</NavLink>
+					</ul>
+				</div>
+				<div
+					className="dark-overlay"
+					onClick={toggleSideBar}
+					style={sideBar ? { opacity: "100%" } : { opacity: "0%", display: "none" }}
+				/>
+			</div>
+		</nav>
+	);
 }
 
 export default Navbar;
