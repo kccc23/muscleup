5ef30ca190a48b5bc7939322872c770caebbf777
Charles Stephens <charlesstephens13@gmail.com>
2023-06-07 14:20:06 -0700

    created the Hero page

--- ghi/src/components/mainpage/Main.js
+++ ghi/src/components/mainpage/Main.js
@@ -1,13 +1,29 @@
+import "./main.css";
 import React from "react";
 
-
 function Main() {
-
-	return (
-		<div>
-			
-		</div>
-	);
+return (
+        <div className="hero-page" style={{ height: "100%" }}>
+			<div className="img-container">
+				<section className="hero-component">
+						<div className="hero-paragraph">
+							<p className="hero-p-tag">
+								From Lawn Mowing to Iron Man
+								<br />
+								From Seaweed to Turducken
+								<br />
+								We Help you Track
+								<br />
+								Meet your Unique Goals
+								<br />
+								Keep you Challenged and Engaged
+							</p>
+							<a href="/signup" className="hero-sign-up">Muscle Up!</a>
+						</div>
+					</section>
+			</div>
+        </div>
+    );
 }
 
 export default Main;
