f9fa1c6120ab089521022ff73973071c38ee4bff
Yosef <yosef.dandis@gmail.com>
2023-06-06 21:20:34 -0500

    deployment test 8

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Main from "./components/mainpage/Main";
 import Navbar from "./components/navbar/Navbar";
-import LogInModal from "./components/Modal/LoginModal";
 import SignUp from "./components/signup/SignUp";
 import ProfileCreate from "./components/signup/ProfileCreate";
 import Dashboard from "./components/dashboard/DashboardPage";
